0682ffca69020b9b6dc720eb20c94d6bdfa88e46
hugoAvallay <h40hugo@gmail.com>
2023-05-22 22:43:24 -0300

    renombre los archivos y agregue un poco de codigo

---
+++ archivo4.js
@@ -0,0 +1,5 @@
+const saludar = function(nombre){
+    return "Hola soy " + nombre
+}
+
+console.log(saludar("hugo"));
---
+++ archivo6.js
@@ -0,0 +1,13 @@
+var mascota = {
+    animal: 'Perro',
+    raza: 'Ovejero Alemán',
+    amistoso: true,
+    dueño: 'María López',
+};
+const detalle = () => {
+    console.log('Mi perro es un  ' + mascota.raza);
+}
+
+
+
+detalle(); //llamo a la funcion para ver una propiedad del objeto mascota
